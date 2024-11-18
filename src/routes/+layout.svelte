0fc6ad66d1bdd22be0957bea81499ab4b375e9dd
Marcus Cvjeticanin <mjovanc@icloud.com>
2024-11-18 12:42:07 +0100

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
   import Navbar from '$lib/components/Navbar.svelte';
   import Footer from '$lib/components/Footer.svelte';
   import '../app.css';
-  let pageTitle = 'Njord - A lightweight ORM library for Rust';
+  let pageTitle = 'Njord - A versatile, feature-rich Rust ORM';
 </script>
 
 <svelte:head>
@@ -11,14 +11,14 @@
   <meta property="og:title" content={pageTitle} />
   <meta
     property="og:description"
-    content="A lightweight and extensible ORM library and Query Builder for Rust."
+    content="A highly versatile and feature-rich ORM library for Rust, designed to simplify database interactions across a wide range of systems with robust performance and flexibility."
   />
   <meta property="og:image" content="{base}/meta.png" />
   <meta property="twitter:card" content="summary_large_image" />
   <meta property="twitter:title" content={pageTitle} />
   <meta
     property="twitter:description"
-    content="A lightweight and extensible ORM library and Query Builder for Rust."
+    content="A highly versatile and feature-rich ORM library for Rust, designed to simplify database interactions across a wide range of systems with robust performance and flexibility."
   />
   <meta property="twitter:image" content="{base}/meta.png" />
 
